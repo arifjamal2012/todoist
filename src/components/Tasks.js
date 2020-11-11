5f64b021d3541dc5d3b35d150ef76d9419b2dbf9
arifjamal2012 <arifjamal2012@gmail.com>
2020-11-11 20:18:34 +0530

    added inbox

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Checkbox } from './Checkbox';
 import { useTasks } from '../hooks';
+import { collectedTasks } from '../constants';
+import { getTitle, getCollectedTitle, colletedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Tasks = () => {
-	const { tasks } = useTasks('1');
-
-	console.log(tasks);
+	const { selectedProject } = useSelectedProjectValue();
+	const { projects } = useProjectsValue();
+	const { tasks } = useTasks(selectedProject);
 
 	let projectName = '';
+
+	if (projects && selectedProject && !colletedTasksExist(selectedProject)) {
+		projectName = getTitle(projects, selectedProject).name;
+		console.log('projectName 1', projectName);
+	}
+	if (colletedTasksExist(selectedProject) && selectedProject) {
+		projectName = getCollectedTitle(collectedTasks, selectedProject).name;
+		console.log('projectName 2', projectName);
+	}
+
+	useEffect(() => {
+		document.title = `${projectName}:Todoist`;
+	});
+
+	console.log('tasks', tasks);
 
 	return (
 		<div className='tasks' data-testid='tasks'>
